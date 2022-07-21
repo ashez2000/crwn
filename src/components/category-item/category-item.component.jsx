160060b4a862154df1e1d6009d0812cb86d7440d
Aswin Gangadharan <aswin.gangadharan.2020@gmail.com>
2022-07-21 16:00:27 +0530

    add netlify redirect and fix UI

--- src/components/category-item/category-item.component.jsx
+++ src/components/category-item/category-item.component.jsx
@@ -1,8 +1,12 @@
+import { Link } from 'react-router-dom'
+
 const CategoryItem = ({ category }) => {
   return (
     <div className="card w-100 d-flex flex-column justify-content-center align-items-center my-3 p-5">
       <h3>{category.title}</h3>
-      <span>Shop now</span>
+      <Link to={`/shop/${category.title}`}>
+        <span>Shop now</span>
+      </Link>
     </div>
   )
 }
