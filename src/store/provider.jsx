ad72d7b953795427a88e86b3177428b3c72f8313
Aswin Gangadharan <aswin.gangadharan.2020@gmail.com>
2022-07-30 13:19:46 +0530

    add redux persist

--- src/store/provider.jsx
+++ src/store/provider.jsx
@@ -1,18 +1,55 @@
+import { combineReducers } from 'redux'
 import { configureStore } from '@reduxjs/toolkit'
 import { Provider } from 'react-redux'
+import {
+  persistReducer,
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist'
+import { PersistGate } from 'redux-persist/integration/react'
+import storage from 'redux-persist/lib/storage'
 
 import authReducer from './auth/auth.slice'
 import collectionReducer from './collection/collection.slice'
 import cartReducer from './cart/cart.slice'
 
-const store = configureStore({
-  reducer: {
-    auth: authReducer,
-    collection: collectionReducer,
-    cart: cartReducer,
-  },
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['cart'],
+}
+
+const rootReducer = combineReducers({
+  auth: authReducer,
+  collection: collectionReducer,
+  cart: cartReducer,
 })
 
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+})
+
+const persistor = persistStore(store)
+
 export default ({ children }) => {
-  return <Provider store={store}>{children}</Provider>
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        {children}
+      </PersistGate>
+    </Provider>
+  )
 }
