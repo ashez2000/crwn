0de2cde88ecd28eb43b631b502a1f25eecc8fd04
Aswin Gangadharan <aswin.gangadharan.2020@gmail.com>
2023-06-17 20:35:06 +0530

    Fetch products

---
+++ src/app/products/page.jsx
@@ -0,0 +1,18 @@
+import { db } from '../../db/conn'
+
+export default async function Products() {
+    const products = await db.product.findMany()
+
+    return (
+        <div>
+            <h1>Products</h1>
+            <ul>
+                {products.map((product) => (
+                    <li key={product.id}>
+                        {product.name} - {product.price}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
+}
