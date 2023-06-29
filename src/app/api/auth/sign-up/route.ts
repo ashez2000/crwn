1e61fbf839bc896a17857776541f3094a5b703dd
Aswin Gangadharan <aswin.gangadharan.2020@gmail.com>
2023-06-29 05:46:22 +0530

    Update sign-up and sign-in

--- src/app/api/auth/sign-up/route.ts
+++ src/app/api/auth/sign-up/route.ts
@@ -5,7 +5,14 @@ export async function POST(req: Request) {
   const { name, email, password } = await req.json()
   const hash = await argon.hash(password)
 
-  const user = await db.user.create({
+  const exist = await db.user.findUnique({ where: { email } })
+  if (exist) {
+    return new Response(JSON.stringify({ message: 'Email already exist' }), {
+      status: 400,
+    })
+  }
+
+  await db.user.create({
     data: {
       name,
       email,
@@ -13,5 +20,5 @@ export async function POST(req: Request) {
     },
   })
 
-  return new Response(JSON.stringify(user), { status: 201 })
+  return new Response(null, { status: 201 })
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,29 +1,31 @@
+import { Toaster } from 'react-hot-toast'
+
 import './bootstrap.css'
 import './global.css'
 
 import Provider from './provider'
-
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
 
 export const metadata = {
-    title: 'CRWN | Clothing Store',
+  title: 'CRWN | Clothing Store',
 }
 
-type RootLayoutProps = {
-    children: React.ReactNode
+type Props = {
+  children: React.ReactNode
 }
 
-export default function RootLayout({ children }: RootLayoutProps) {
-    return (
-        <html lang="en">
-            <body>
-                <Provider>
-                    <Header />
-                    <main className="container">{children}</main>
-                    <Footer />
-                </Provider>
-            </body>
-        </html>
-    )
+export default function RootLayout({ children }: Props) {
+  return (
+    <html lang="en">
+      <body>
+        <Toaster />
+        <Provider>
+          <Header />
+          <main className="container">{children}</main>
+          <Footer />
+        </Provider>
+      </body>
+    </html>
+  )
 }
