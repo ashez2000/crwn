05ca764a6671653e0120b9d7125c2e003171fc8b
Aswin Gangadharan <aswin.gangadharan.2020@gmail.com>
2023-06-29 17:14:44 +0530

    Add local category imgs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,46 +1,20 @@
-import CategoryItem from '@/components/CategoryItem'
+import CategoryItem from '@/components/category/CategoryItem'
 
-const categories = [
-  {
-    id: 1,
-    title: 'hats',
-    image: 'https://i.ibb.co/cvpntL1/hats.png',
-  },
-  {
-    id: 2,
-    title: 'jackets',
-    image: 'https://i.ibb.co/px2tCc3/jackets.png',
-  },
-  {
-    id: 3,
-    title: 'sneakers',
-    image: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-  },
-  {
-    id: 4,
-    title: 'womens',
-    image: 'https://i.ibb.co/GCCdy8t/womens.png',
-  },
-  {
-    id: 5,
-    title: 'mens',
-    image: 'https://i.ibb.co/R70vBrQ/men.png',
-  },
-]
+const categories = ['hats', 'jackets', 'sneakers', 'womens', 'mens']
 
 export default function Home() {
   return (
     <div>
       <div className="d-grid gap-3">
         <div className="row row-cols-3">
-          <CategoryItem item={categories[0]} />
-          <CategoryItem item={categories[1]} />
-          <CategoryItem item={categories[2]} />
+          <CategoryItem title={categories[0]} />
+          <CategoryItem title={categories[1]} />
+          <CategoryItem title={categories[2]} />
         </div>
 
         <div className="row row-cols-2">
-          <CategoryItem item={categories[3]} size="lg" />
-          <CategoryItem item={categories[4]} size="lg" />
+          <CategoryItem title={categories[3]} size="lg" />
+          <CategoryItem title={categories[4]} size="lg" />
         </div>
       </div>
     </div>
