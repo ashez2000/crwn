db8406e0cc410320db8878d6a4ed4ac6c1ab23e5
Aswin Gangadharan <aswin.gangadharan.2020@gmail.com>
2023-06-29 16:36:07 +0530

    Handle quantity for cart

--- src/context/CartContext.tsx
+++ src/context/CartContext.tsx
@@ -3,57 +3,78 @@
 import { Product } from '@prisma/client'
 import { createContext, useState, useContext, useEffect } from 'react'
 
+export type CartItem = Product & { quantity: number }
+
 type CartContextType = {
-    cart: Product[]
-    addToCart: (product: Product) => void
-    removeFromCart: (product: Product) => void
-    clearCart: () => void
+  cartItems: CartItem[]
+  addToCart: (product: Product) => void
+  removeFromCart: (id: string) => void
+  clearCart: () => void
 }
 
 const CartContext = createContext<CartContextType>({} as CartContextType)
 
-export const useCart = () => useContext(CartContext)
-
 export const CartProvider = ({ children }: { children: React.ReactNode }) => {
-    const [cart, setCart] = useState<Product[]>([])
+  const [cartItems, setCartItems] = useState<CartItem[]>([])
 
-    useEffect(() => {
-        const cart = localStorage.getItem('cart')
-        if (cart) {
-            setCart(JSON.parse(cart))
-        }
-    }, [])
-
-    useEffect(() => {
-        localStorage.setItem('cart', JSON.stringify(cart))
-    }, [cart])
-
-    const addToCart = (product: Product) => {
-        const exist = cart.find((item) => item.id === product.id)
-        if (!exist) {
-            setCart([...cart, product])
-        }
+  useEffect(() => {
+    const cart = localStorage.getItem('cart')
+    if (cart) {
+      setCartItems(JSON.parse(cart))
     }
+  }, [])
 
-    const removeFromCart = (product: Product) => {
-        const newCart = cart.filter((item) => item.id !== product.id)
-        setCart(newCart)
+  useEffect(() => {
+    localStorage.setItem('cart', JSON.stringify(cartItems))
+  }, [cartItems])
+
+  const addToCart = (product: Product) => {
+    const exist = cartItems.find((item) => item.id === product.id)
+    if (exist) {
+      const newCart = cartItems.map((item) =>
+        item.id === product.id ? { ...item, quantity: item.quantity + 1 } : item
+      )
+      setCartItems(newCart)
+    } else {
+      setCartItems([...cartItems, { ...product, quantity: 1 }])
     }
+  }
 
-    const clearCart = () => {
-        setCart([])
+  const removeFromCart = (id: string) => {
+    const exist = cartItems.find((item) => item.id === id)
+    if (exist) {
+      if (exist.quantity === 1) {
+        clearFromCart(id)
+      } else {
+        const newCart = cartItems.map((item) =>
+          item.id === id ? { ...item, quantity: item.quantity - 1 } : item
+        )
+        setCartItems(newCart)
+      }
     }
+  }
 
-    return (
-        <CartContext.Provider
-            value={{
-                cart,
-                addToCart,
-                removeFromCart,
-                clearCart,
-            }}
-        >
-            {children}
-        </CartContext.Provider>
-    )
+  const clearFromCart = (id: string) => {
+    const newCart = cartItems.filter((item) => item.id !== id)
+    setCartItems(newCart)
+  }
+
+  const clearCart = () => {
+    setCartItems([])
+  }
+
+  return (
+    <CartContext.Provider
+      value={{
+        cartItems,
+        addToCart,
+        removeFromCart,
+        clearCart,
+      }}
+    >
+      {children}
+    </CartContext.Provider>
+  )
 }
+
+export const useCart = () => useContext(CartContext)
