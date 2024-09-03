fd2e51bbccbc34ebed6fa026d2e99bf590ecffcf
Aswin Gangadharan <aswin.gangadharan.2020@gmail.com>
2024-09-03 16:15:01 +0530

    server: Save user data to db

--- server/src/http/routes/auth.ts
+++ server/src/http/routes/auth.ts
@@ -1,19 +1,69 @@
 import { Router } from 'express'
+import bcrypt from 'bcryptjs'
+
+import prisma from '../../lib/prisma.js'
 
 const routes = Router()
 
+// route: POST /auth/signup
 routes.post('/signup', async (req, res) => {
+  // TODO: validation
   const { name, email, password, role } = req.body
-  res.status(201).json({ message: 'signup user' })
+
+  const emailTaken = await findUserByEmail(email)
+  if (emailTaken) {
+    return res.status(400).json({
+      message: 'email already exist',
+    })
+  }
+
+  const hash = await bcrypt.hash(password, 12)
+  const user = await prisma.user.create({
+    data: { name, email, password: hash, role },
+  })
+
+  res.status(201).json({
+    id: user.id,
+    name: user.name,
+    email: user.email,
+  })
 })
 
+async function findUserByEmail(email: string) {
+  return prisma.user.findUnique({
+    where: { email },
+  })
+}
+
+// route: POST /auth/signin
 routes.post('/signin', async (req, res) => {
+  // TODO: validation
   const { email, password } = req.body
-  res.status(201).json({ message: 'signin user' })
+
+  const user = await findUserByEmail(email)
+  if (!user) {
+    return res.status(401).json({
+      message: 'invalid credentials',
+    })
+  }
+
+  const isMatch = bcrypt.compare(password, user.password)
+  if (!isMatch) {
+    return res.status(401).json({
+      message: 'invalid credentials',
+    })
+  }
+
+  res.status(200).json({
+    id: user.id,
+    name: user.name,
+    email: user.email,
+  })
 })
 
+// route: POST /auth/signout
 routes.post('/signout', async (req, res) => {
-  res.status(201).json({ message: 'signout user' })
+  res.status(200).json({})
 })
 
 export default routes
