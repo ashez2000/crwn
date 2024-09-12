2edfac81ce22d1f8fc02cd021199355e2d5863a2
Aswin Gangadharan <aswin.gangadharan.2020@gmail.com>
2024-09-12 08:44:28 +0530

    Render products

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,15 @@
-import { db } from '@/lib/prisma'
+import axios from 'axios'
 import ProductCard from '@/components/product-card'
 
 type Props = {}
 
 export default async function RootPage({}: Props) {
-  const products = await db.product.findMany()
+  const { data: products } = await axios.get('http://localhost:3001/products')
 
   return (
     <>
       <div className="grid grid-cols-4 gap-3">
-        {products.map(p => (
+        {products.map((p: any) => (
           <ProductCard key={p.id} product={p} />
         ))}
       </div>
